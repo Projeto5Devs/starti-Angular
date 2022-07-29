2714176ef220eec0061340e0947d1b8bdc8216df
Dayane Leal <dayane_leal2004@hotmail.com>
2022-07-29 16:40:36 -0300

    Criação da Página de Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,26 @@ export class AppComponent implements OnInit  {
 
   ngOnInit(): void {
 
+    $(() =>  {
+
+      function adjustFooter() {
+          var footer = $("footer");
+
+          if ($(document).height() > $(window).height()) {
+              footer.css("position", "inherit");
+          } else {
+              footer.css({"position": "absolute", "bottom": "0"});
+          }
+      }
+
+      window.addEventListener('resize',function() {
+          adjustFooter();
+      });
+
+      adjustFooter();
+
+  });
+
     const select = (el, all = false) => {
       el = el.trim()
       if (all) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,14 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
