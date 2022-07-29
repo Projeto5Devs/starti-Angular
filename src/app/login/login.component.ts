ce326f71cac7a2b91227fa32967cd57aa3baf9bb
Dayane Leal <dayane_leal2004@hotmail.com>
2022-07-29 17:58:15 -0300

    Página de Login - continuação

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,12 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+   document.getElementById ("footer").style.display = "none";
+   document.querySelector("nav").style.display = "none";
+
+   document.querySelector("body").style.cssText ="display: flex; justify-content: center; align-items: center; background-color: rgba(79, 79, 79, 0.037);"
+
   }
 
 }
