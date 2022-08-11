6c3644e9bec707f1232f2cdf26440512109cdcf4
barbaradsp <barbaradspimenta@gmail.com>
2022-08-11 20:20:44 -0300

    Atualização da página de edição de informações de candidato

--- src/app/candidato/candidato.module.ts
+++ src/app/candidato/candidato.module.ts
@@ -1,9 +1,11 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CandidatoRoutingModule } from './candidato-routing.module';
 import { PerfilComponent } from './perfil/perfil.component';
 import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
   ],
   imports: [
     CommonModule,
-    CandidatoRoutingModule
+    CandidatoRoutingModule,
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class CandidatoModule { }
