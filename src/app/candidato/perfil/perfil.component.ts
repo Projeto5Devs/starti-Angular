ad6101816baea05a2b5f65b4471b66422f1d205d
Dayane Leal <dayane_leal2004@hotmail.com>
2022-08-15 14:41:43 -0300

    Modal de confirmação de exclusão da inscrição na vaga. Alerta de inscrição realizada com sucesso.

--- src/app/candidato/perfil/perfil.component.ts
+++ src/app/candidato/perfil/perfil.component.ts
@@ -1,10 +1,12 @@
 import { CandidatoService } from './../service/candidato.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {Location} from '@angular/common';
 import { NavbarService } from 'src/app/template/navbar/navbar.service';
 import { UserService } from 'src/app/user/user.service';
 import { InscricaoService } from 'src/app/vagas/inscricao/inscricao.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-perfil',
@@ -13,12 +15,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class PerfilComponent implements OnInit {
 
+  deleteModalRef: BsModalRef;
+  @ViewChild('deletarModal') deletarModal;
   form: FormGroup;
   id: number
   idCandidato: number
   vagas: any
+  idVagaDeletar: number
 
-  constructor(private _location: Location, public nav: NavbarService, private userService:UserService, private inscricao :InscricaoService, private formBuilder: FormBuilder, private candidato: CandidatoService) {
+  constructor(private _location: Location, public nav: NavbarService, private userService:UserService, private inscricao :InscricaoService, private formBuilder: FormBuilder, private candidato: CandidatoService, private modalService: BsModalService) {
     this.form = this.formBuilder.group({
       vagas: ['']
     });
@@ -45,11 +50,21 @@ export class PerfilComponent implements OnInit {
   }
 
   remover(idVaga: number){
-    console.log(idVaga)
-    this.inscricao.deletarPorIdVaga(idVaga).subscribe((data)=>{
-      console.log("success");
- });
+    this.deleteModalRef = this.modalService.show(this.deletarModal, {class: 'modal-sm'})
+    this.idVagaDeletar = idVaga
+  }
+
+  onConfirmDelete(){
+    this.inscricao.deletarPorIdVaga(this.idVagaDeletar).subscribe((data)=>{
+      console.log("Inscrição removida com sucesso.");
+    }, error => {
+      console.log("Não foi possível remover a inscrição.");
+    });
     window.location.reload()
+  }
+
+  onDeclineDelete(){
+    this.deleteModalRef.hide()
   }
 
 }
